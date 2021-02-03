eb6517fe7603523f632ceffa0498153e32a7f7d1
Andreas Amstutz <tullo@users.noreply.github.com>
2021-02-04 00:08:32 +0100

    fix event consumation

--- shippy-service-email/go.mod
+++ shippy-service-email/go.mod
@@ -3,15 +3,7 @@ module github.com/tullo/shippy/shippy-service-email
 go 1.15
 
 require (
-	github.com/google/go-cmp v0.5.4 // indirect
 	github.com/micro/micro/v3 v3.0.4
 	github.com/mwitkow/go-conntrack v0.0.0-20190716064945-2f068394615f // indirect
-	github.com/nxadm/tail v1.4.6 // indirect
-	github.com/onsi/ginkgo v1.15.0 // indirect
-	github.com/onsi/gomega v1.10.5 // indirect
-	github.com/tullo/shippy/shippy-service-user v0.0.0-20210203160205-fce11f5dbcd9
-	golang.org/x/sync v0.0.0-20201207232520-09787c993a3a // indirect
-	google.golang.org/genproto v0.0.0-20210203152818-3206188e46ba // indirect
-	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
-	gopkg.in/yaml.v2 v2.4.0 // indirect
+	github.com/tullo/shippy/shippy-service-user v0.0.0-20210203212848-1633a4eb9a0a
 )
--- shippy-service-email/main.go
+++ shippy-service-email/main.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"context"
-	"log"
-
 	"github.com/micro/micro/v3/service"
+	"github.com/micro/micro/v3/service/events"
 	"github.com/micro/micro/v3/service/logger"
 	proto "github.com/tullo/shippy/shippy-service-user/proto"
 )
@@ -12,20 +10,50 @@ import (
 const topic = "user.created"
 
 // Process sends emails to created users.
-func Process(ctx context.Context, user *proto.User) error {
-	log.Println("Picked up a new message")
-	log.Println("Sending email to:", user.Name)
+func process() error {
+	evStream, err := events.Consume(topic)
+	if err != nil {
+		logger.Errorf("error streaming events: topic: %v. error: %v", topic, err)
+		return err
+	}
 
-	return nil
+	for {
+		select {
+		case ev := <-evStream:
+			// Recieved a message.
+			// Unmarshal it into a message struct.
+			// If an error occurs log it.
+			var u proto.User
+			if err := ev.Unmarshal(&u); err != nil {
+				logger.Errorf("error unmarshaling message. topic:%v. error:%v", topic, err)
+				return err
+			}
+
+			logger.Infof("Picked up a new message: ID:", ev.ID)
+			logger.Infof("Sending email to:", u.Name)
+		}
+	}
 }
 
 func main() {
 	srv := service.New(service.Name("shippy.service.email"))
 	srv.Init()
 
-	// subscribe to the topic
-	service.Subscribe(topic, Process)
-	if err := srv.Run(); err != nil {
-		logger.Fatal(err)
+	done := make(chan error, 2)
+	go func() {
+		done <- process()
+	}()
+
+	go func() {
+		if err := srv.Run(); err != nil {
+			logger.Fatal(err)
+			done <- err
+		}
+	}()
+
+	for i := 0; i < cap(done); i++ {
+		if err := <-done; err != nil {
+			logger.Errorf("error: %v", err)
+		}
 	}
 }
